80af679f855f9d0e4b2554de29eb9dfd3f057c67
Pat Thomas <pthomas@relaynetwork.com>
2014-09-30 23:10:52 -0400

    Stable version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-utils "0.2.0-SNAPSHOT"
+(defproject om-utils "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
