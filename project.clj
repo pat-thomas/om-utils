d08a55165f5ef71816b85f3cffcdfe37adea02db
Pat Thomas <pthomas@relaynetwork.com>
2014-09-30 23:12:01 -0400

    0.2.0 -> 0.3.0-SNAPSHOT for development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-utils "0.2.0"
+(defproject om-utils "0.3.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
