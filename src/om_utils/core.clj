1acfe6939ea59369c6e27fab5627113c81ea5faa
Pat Thomas <pthomas@relaynetwork.com>
2014-10-08 23:57:53 -0400

    Move large maps of data into their own namespace.

--- src/om_utils/core.clj
+++ src/om_utils/core.clj
@@ -1,29 +1,6 @@
 (ns om-utils.core
-  (:require [clojure.walk :as walk]))
-
-(def lookup-table
-  {'did-mount          {:lifecycle-method 'om.core/IDidMount
-                        :arg-list         '[this]}
-   'did-update         {:lifecycle-method 'om.core/IDidUpdate
-                        :arg-list         '[this prev-props prev-state]}
-   'display-name       {:lifecycle-method 'om.core/IDisplayName
-                        :arg-list         '[this]}
-   'init-state         {:lifecycle-method 'om.core/IInitState
-                        :arg-list         '[this]}
-   'render             {:lifecycle-method 'om.core/IRender
-                        :arg-list         '[this]}
-   'render-state       {:lifecycle-method 'om.core/IRenderState
-                        :arg-list         '[this state]}
-   'should-update      {:lifecycle-method 'om.core/IShouldUpdate
-                        :arg-list         '[this next-props next-state]}
-   'will-mount         {:lifecycle-method 'om.core/IWillMount
-                        :arg-list         '[this]}
-   'will-receive-props {:lifecycle-method 'om.core/IWillReceiveProps
-                        :arg-list         '[this next-state]}
-   'will-unmount       {:lifecycle-method 'om.core/IWillUnmount
-                        :arg-list         '[this]}
-   'will-update        {:lifecycle-method 'om.core/IWillUpdate
-                        :arg-list         '[this next-props next-state]}})
+  (:require [om-utils.data :as data]
+            [clojure.walk  :as walk]))
 
 (defn make-friendly-display-name
   [component-name]
@@ -43,7 +20,7 @@
   [component-name body]
   (let [reify-lifecycle-methods (map (fn [expr]
                                        (let [impl-fn-name (first expr)]
-                                         (get-in lookup-table [impl-fn-name :lifecycle-method])))
+                                         (get-in data/lookup-table [impl-fn-name :lifecycle-method])))
                                      body)
         body-with-auto-exprs    (map (fn [expr]
                                        (let [impl-fn-name (first expr)]
@@ -59,142 +36,13 @@
     `(reify
        ~@reify-body)))
 
-(def dom-fn-replacement-map
-  (reduce (fn [acc dom-fn]
-            (assoc acc dom-fn (symbol (str 'om.dom "/" dom-fn))))
-          {}
-          '[a
-            abbr
-            address
-            area
-            article
-            aside
-            audio
-            b
-            base
-            bdi
-            bdo
-            big
-            blockquote
-            body
-            br
-            button
-            canvas
-            caption
-            cite
-            code
-            col
-            colgroup
-            data
-            datalist
-            dd
-            del
-            dfn
-            div
-            dl
-            dt
-            em
-            embed
-            fieldset
-            figcaption
-            figure
-            footer
-            form
-            h1
-            h2
-            h3
-            h4
-            h5
-            h6
-            head
-            header
-            hr
-            html
-            i
-            iframe
-            img
-            ins
-            kbd
-            keygen
-            label
-            legend
-            li
-            link
-            main
-            map
-            mark
-            marquee
-            menu
-            menuitem
-            meta
-            meter
-            nav
-            noscript
-            object
-            ol
-            optgroup
-            output
-            p
-            param
-            pre
-            progress
-            q
-            rp
-            rt
-            ruby
-            s
-            samp
-            script
-            section
-            select
-            small
-            source
-            span
-            strong
-            style
-            sub
-            summary
-            sup
-            table
-            tbody
-            td
-            tfoot
-            th
-            thead
-            time
-            title
-            tr
-            track
-            u
-            ul
-            var
-            video
-            wbr
-            
-            ;; svg
-            circle
-            ellipse
-            g
-            line
-            path
-            polyline
-            rect
-            svg
-            text
-            defs
-            linearGradient
-            polygon
-            radialGradient
-            stop
-            tspan]))
-
 (defn autogen-dom-fns
   [fn-body]
   (let [render-methods (filter (fn [expr]
                                  (or (= (first expr) 'render)
                                      (= (first expr) 'render-state)))
                                fn-body)]
-    (walk/postwalk-replace dom-fn-replacement-map render-methods)))
+    (walk/postwalk-replace data/dom-fn-replacement-map render-methods)))
 
 (defn process-body
   [component-name body]
---
+++ src/om_utils/data.clj
@@ -0,0 +1,154 @@
+(ns om-utils.data)
+
+(def lookup-table
+  {'did-mount          {:lifecycle-method 'om.core/IDidMount
+                        :arg-list         '[this]}
+   'did-update         {:lifecycle-method 'om.core/IDidUpdate
+                        :arg-list         '[this prev-props prev-state]}
+   'display-name       {:lifecycle-method 'om.core/IDisplayName
+                        :arg-list         '[this]}
+   'init-state         {:lifecycle-method 'om.core/IInitState
+                        :arg-list         '[this]}
+   'render             {:lifecycle-method 'om.core/IRender
+                        :arg-list         '[this]}
+   'render-state       {:lifecycle-method 'om.core/IRenderState
+                        :arg-list         '[this state]}
+   'should-update      {:lifecycle-method 'om.core/IShouldUpdate
+                        :arg-list         '[this next-props next-state]}
+   'will-mount         {:lifecycle-method 'om.core/IWillMount
+                        :arg-list         '[this]}
+   'will-receive-props {:lifecycle-method 'om.core/IWillReceiveProps
+                        :arg-list         '[this next-state]}
+   'will-unmount       {:lifecycle-method 'om.core/IWillUnmount
+                        :arg-list         '[this]}
+   'will-update        {:lifecycle-method 'om.core/IWillUpdate
+                        :arg-list         '[this next-props next-state]}})
+
+(def dom-fn-replacement-map
+  (reduce (fn [acc dom-fn]
+            (assoc acc dom-fn (symbol (str 'om.dom "/" dom-fn))))
+          {}
+          '[a
+            abbr
+            address
+            area
+            article
+            aside
+            audio
+            b
+            base
+            bdi
+            bdo
+            big
+            blockquote
+            body
+            br
+            button
+            canvas
+            caption
+            cite
+            code
+            col
+            colgroup
+            data
+            datalist
+            dd
+            del
+            dfn
+            div
+            dl
+            dt
+            em
+            embed
+            fieldset
+            figcaption
+            figure
+            footer
+            form
+            h1
+            h2
+            h3
+            h4
+            h5
+            h6
+            head
+            header
+            hr
+            html
+            i
+            iframe
+            img
+            ins
+            kbd
+            keygen
+            label
+            legend
+            li
+            link
+            main
+            map
+            mark
+            marquee
+            menu
+            menuitem
+            meta
+            meter
+            nav
+            noscript
+            object
+            ol
+            optgroup
+            output
+            p
+            param
+            pre
+            progress
+            q
+            rp
+            rt
+            ruby
+            s
+            samp
+            script
+            section
+            select
+            small
+            source
+            span
+            strong
+            style
+            sub
+            summary
+            sup
+            table
+            tbody
+            td
+            tfoot
+            th
+            thead
+            time
+            title
+            tr
+            track
+            u
+            ul
+            var
+            video
+            wbr
+            
+            ;; svg
+            circle
+            ellipse
+            g
+            line
+            path
+            polyline
+            rect
+            svg
+            text
+            defs
+            linearGradient
+            polygon
+            radialGradient
+            stop
+            tspan]))
