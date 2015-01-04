71221d68051a535b29b9832ddf91f7e9af2512bf
Pat Thomas <patthomassoftware@gmail.com>
2015-01-04 17:01:32 -0500

    0.4.0 stable release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-utils "0.4.0-SNAPSHOT"
+(defproject om-utils "0.4.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
