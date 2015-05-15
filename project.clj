ab799e715862ac18a9fc866edaf40214729ea75c
Pat Thomas <pthomas@relaynetwork.com>
2015-05-15 13:15:06 -0400

    Bump to snapshot for development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject om-utils "0.5.0"
+(defproject om-utils "0.6.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
